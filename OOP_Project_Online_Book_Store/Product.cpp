298e44614c736b659e3ac78b4ac8aeb7c969443b
tahirozdemir34 <tahirozdemir34@gmail.com>
2016-12-23 21:13:26 +0300

    Product.cpp

--- OOP_Project_Online_Book_Store/Product.cpp
+++ OOP_Project_Online_Book_Store/Product.cpp
@@ -1,12 +1,38 @@
 #include "Product.h"
 
-
-
-Product::Product()
+int Product::getID() const
 {
+	return ID;
 }
 
-
-Product::~Product()
+void Product::setID(int ID)
 {
+	this->ID = ID;
+}
+
+string Product::getName() const
+{
+	return name;
+}
+
+void Product::setName(const string& name)
+{
+	this->name = name;
+}
+
+double Product::getPrice() const
+{
+	return price;
+}
+
+void Product::setPrice(double price)
+{
+	this->price = price;
+}
+
+void Product::printProperties()const
+{
+	cout << "The Product's ID is: " << ID << endl;
+	cout << "The Product's Name is: " << name << endl;
+	cout << "The Product's Price is: " << price << endl;
 }
