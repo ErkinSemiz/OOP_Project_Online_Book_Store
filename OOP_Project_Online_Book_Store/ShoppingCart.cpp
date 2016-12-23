71f34566d1bfa3f3c3150a41a617954797c20b37
ErkinSemiz <erkin1semiz@gmail.com>
2016-12-23 22:20:42 +0300

    Shopping Cart Getters And Setters Are Ready

--- OOP_Project_Online_Book_Store/ShoppingCart.cpp
+++ OOP_Project_Online_Book_Store/ShoppingCart.cpp
@@ -14,3 +14,28 @@ ShoppingCart::~ShoppingCart()
 		productsToPurchase.pop_front();
 	}
 }
+
+void ShoppingCart::setPaymentMethod(Payment* const payment_method)
+{
+	paymentMethod = payment_method;
+}
+
+void ShoppingCart::setCustomer(Customer* const customer)
+{
+	this->customer = customer;
+}
+
+void ShoppingCart::setBonusUsed(const bool is_bonus_used)
+{
+	isBonusUsed = is_bonus_used;
+}
+
+Payment* const& ShoppingCart::getPaymentMethod() const
+{
+	return paymentMethod;
+}
+
+Customer* const& ShoppingCart::getCustomer() const
+{
+	return customer;
+}
